7b2af712e25dd68244067386d4e40444eb294f9b
Bárbara <babyr.oliveira@gmail.com>
2020-05-19 17:55:05 -0300

    Resolução do exercício.

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace JurosSimples
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Olá usuário, vamos calcular o montante final de um investimento a juros simples.");
+            Console.WriteLine( " Pressione enter para continuar.");
+            Console.ReadKey();
+            
+            Console.WriteLine("Indique a capital:");
+            decimal c = decimal.Parse (Console.ReadLine());
+            Console.WriteLine("Indique a taxa de juros (%):");
+            decimal i = decimal.Parse (Console.ReadLine());
+            Console.WriteLine("Indique o tempo em meses:");
+            decimal t = decimal.Parse (Console.ReadLine());
+            
+            decimal resultj = c * (i/100) * t;
+            decimal resultm = c + resultj ;
+
+            Console.WriteLine ("Juros (R$): " + resultj);
+            Console.WriteLine ("Montante (R$): " + resultm);
+
+            Console.ReadKey();
+
+        }
+    }
+}
